15684e31325fe3af82c1bc91562eb5542e0b0e16
Minseong-Choi <joint0208@naver.com>
2025-07-19 16:47:28 +0900

    fix: error(not everything)

--- background.js
+++ background.js
@@ -1,7 +1,16 @@
-chrome.action.onClicked.addListener(async(tab) => {
-    try{
-        chrome.tabs.sendMessage(tab.id, {type: "TOGGLE_SIDEBAR"});
-    }catch(e){
-        console.error("Error executing script:", e);
+chrome.action.onClicked.addListener(async (tab) => {
+    try {
+      chrome.tabs.sendMessage(tab.id, { type: "TOGGLE_SIDEBAR" }, async () => {
+        if (chrome.runtime.lastError) {
+          await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ["content.js"],
+          });
+
+          chrome.tabs.sendMessage(tab.id, { type: "TOGGLE_SIDEBAR" });
+        }
+      });
+    } catch (e) {
+      console.error("Error executing content script:", e);
     }
-})
+  });
